4dfe7ffbb248c8da4eaa8e03bbe75ec58d947ca8
sandro132 <mianveo@hotmail.com>
2023-05-15 12:50:12 -0500

    new tester

--- monty.c
+++ monty.c
@@ -1,24 +1,62 @@
 #include "monty.h"
 
+/**
+ * main
+ * @argc: Size of argv
+ * @argv: Array of arguments
+ * Return: 0 on success, 1 on failure
+ */
+
 int main(int argc, char **argv)
 {
-    FILE *fp;
-    
+    FILE *file;
 
-    if (argc != 2)
-    {
+    char *line = NULL;
+    size_t lenght_of_line = 0;
+    ssize_t number_of_characters_read = 0;
 
-        fprintf(stderr, "USAGE: monty file");
-        exit (EXIT_FAILURE);
-    }  
-    fp = fopen(argv[1], "r");
-    if(fp == NULL)
+    unsigned int line_number = 1;
+    char *command = NULL;
+
+    stack_t *stack = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+
+	if (file == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+    number_of_characters_read = getline(&line, &lenght_of_line, file);
+
+    if (number_of_characters_read == -1)
+        printf("This File is empty\n");
+
+    if (line_number == 1)
+        fclose(file);
+
+    /* printf("%s\n", line); */
+
+    command = strtok(line, " \n");
+    /*
+        if (command == NULL)
+
+    */
+
+    if (strcmp(command, "push") == 0)
     {
-        fprintf(stderr, "Error: Can't open file %s", argv[1]);
-        exit (EXIT_FAILURE);
+        _push(&stack, line_number);
     }
-    if (r_string(argc, argv))
-    
-}
 
-    
+    if (strcmp(command, "pall") == 0)
+    {
+        _pall(&stack, line_number);
+    }
+
+	return (EXIT_SUCCESS);
+}
--- monty.h
+++ monty.h
@@ -4,24 +4,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <stdarg.h>
+#include <unistd.h>
 
-typedef struct stack
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
 {
-        int n;
-        struct stack *prev;
-        struct stack *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
-
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-int main (int argc, char **argv);
-void push(stack_t **pila, unsigned int line);
-void pall(stack_t **pila);
+extern int error;
+int main(int argc, char **argv);
+stack_t *add_node(stack_t **stack, int n);
+int check_digit(char *arg);
+int _isdigit(int c);
 
-#endif 
+void check_opcode(char *op, stack_t **stack, unsigned int line_number);
+void free_all(stack_t *stack, FILE *fd);
+void _push(stack_t **stack, unsigned int line_number);
+void _pall(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_numer);
+void _nop(stack_t **stack, unsigned int line_number);
+
+#endif
